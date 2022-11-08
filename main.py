f275051c6c0f00969d98195bf8524c0aa24abf3d
Rye <sang.dinh@wizeline.com>
2022-11-08 17:04:08 +0700

    simple api test

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Welcome"}
+
+
+@app.get("/lists")
+async def lists():
+    return {"message": "return list of lists"}
+
+
+@app.get("/list/{list_id}")
+async def get_list(list_id: str):
+    return {"message": list_id}
+
+
+@app.post("/list/}")
+async def post_list():
+    return {"message"}
