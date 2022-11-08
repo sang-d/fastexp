888936f2374a3392f95431c10ce74b151980a296
Rye <sang.dinh@wizeline.com>
2022-11-08 17:23:52 +0700

    test put

--- main.py
+++ main.py
@@ -1,23 +1,39 @@
+from typing import List, Union
+
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class CheckList(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    parent_id: Union[int, None] = None
+    children: List[int] = []
+
 
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
-    return {"message": "Welcome"}
+    return {"res": "live"}
 
 
 @app.get("/lists")
 async def lists():
-    return {"message": "return list of lists"}
+    return {"res": []}
 
 
 @app.get("/list/{list_id}")
 async def get_list(list_id: str):
-    return {"message": list_id}
+    return {"res": list_id}
 
 
 @app.post("/list/")
-async def post_list():
-    return {"message"}
+async def create_list(checklist: CheckList):
+    return {"res": checklist}
+
+
+@app.put("/list/{list_id}")
+async def update_list(list_id: int, checklist: CheckList):
+    return {list_id: checklist}
