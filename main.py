6e0bc5e89c52100150b25f3b41c4d2f07c3bd45a
Rye <sang.dinh@wizeline.com>
2022-11-09 10:29:27 +0700

    update list

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from typing import List, Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, status
 from pydantic import BaseModel
 
 
@@ -14,26 +14,50 @@ class CheckList(BaseModel):
 app = FastAPI()
 
 
+my_check_lists = {
+    "travel": {
+        "documents": ["ticket", "accomodation", "id"],
+        "clothes": ["weather", "jacket"],
+        "activities": ["shoes", "socks", "gloves", "glasses", "windbreaker"],
+        "devices": ["phone", "watch", "charger", "keys"],
+        "maintainance": [
+            "tooth brusher",
+            "razor",
+            "foam",
+            "anti-mosquito",
+            "hankerchief",
+        ],
+        "stuff": ["gift", "other people items", "give away"],
+        "off": ["echo", "hot water", "kitchen"],
+        "on": ["autofeeder", "monitoring"],
+    }
+}
+
+
 @app.get("/")
 async def root():
-    return {"res": "live"}
+    return "live"
 
 
 @app.get("/lists")
 async def lists():
-    return {"res": []}
+    return my_check_lists
 
 
 @app.get("/list/{list_id}")
 async def get_list(list_id: str):
-    return {"res": list_id}
+    if list_id not in my_check_lists:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="item not found"
+        )
+    return my_check_lists[list_id]
 
 
-@app.post("/list/")
-async def create_list(checklist: CheckList):
-    return {"res": checklist}
+@app.post("/list/", status_code=status.HTTP_201_CREATED)
+async def create_list(alist: CheckList):
+    return alist
 
 
 @app.put("/list/{list_id}")
-async def update_list(list_id: int, checklist: CheckList):
-    return {list_id: checklist}
+async def update_list(list_id: str, alist: CheckList):
+    return alist
