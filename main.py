ad0eab3fbacabaf043aa9073f46e1a6d9bf94ee1
Rye <sang.dinh@wizeline.com>
2022-11-11 19:44:28 +0700

    test middleware

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import time
 from typing import List, Union
 
-from fastapi import FastAPI, HTTPException, status
+from fastapi import FastAPI, HTTPException, Request, status
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 
@@ -12,6 +14,26 @@ class CheckList(BaseModel):
 
 
 app = FastAPI()
+
+# app.add_middleware(HTTPSRedirectMiddleware)
+origins = ["http://localhost", "http://localhost:8080"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+    return response
 
 
 my_check_lists = {
