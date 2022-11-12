0152c48a84bb3d22e02591622486b8252abe3979
Rye <sang.dinh@wizeline.com>
2022-11-12 13:13:24 +0700

    add db conn

---
+++ sql_app/database.py
@@ -0,0 +1,20 @@
+import os
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+username = os.environ.get("MYSQL_USERNAME")
+password = os.environ.get("MYSQL_PWD")
+host = os.environ.get("MYSQL_HOST")
+port = os.environ.get("MYSQL_PORT")
+dbname = "fastexp"
+
+SQLALCHEMY_DATABASE_URL = f"mysql://{username}:{password}@{host}:{port}/{dbname}"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL  # , connect_args={"check_same_thread": False}
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
